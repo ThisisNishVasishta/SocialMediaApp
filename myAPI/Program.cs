8976956adc29ca842f046a1ba210b25ee010bb0a
Nishith Vasishta <nishith.vasishtta@siemens-healthineers.com>
2024-03-08 19:23:37 +0530

    WalkingSkeleton1

---
+++ myAPI/Program.cs
@@ -0,0 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+using myAPI.Data;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+        builder.Services.AddDbContext<DataContext>(opt =>
+        {
+            opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+        });
+
+        var app = builder.Build();
+
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}
